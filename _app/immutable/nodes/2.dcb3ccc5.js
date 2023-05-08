3c295b4be98932bf806dcef3b29d2a0959e50ccb
Justin Ahinon <justiny.ahinon@gmail.com>
2023-05-08 12:26:09 +0200

    Updates

---
+++ _app/immutable/nodes/2.dcb3ccc5.js
@@ -0,0 +1,49 @@
+import{S as ii,i as ei,s as ti,a as o,k as a,q as s,J as ui,h as e,c as l,l as n,m as c,r as p,K as Y,n as d,b as u,D as q,H as U}from"../chunks/index.fbfca894.js";function oi($){let f,v,j,D,x,K,w,r,Z,H,b,S,M,h,V,P,_,A,y,L,g,I,m,R,G,E,F,Q,C,T;return{c(){f=o(),v=a("h1"),j=s("How to deploy a SvelteKit website to GitHub pages"),D=o(),x=a("p"),K=s(`Dolore ipsum esse voluptate. Culpa officia minim duis enim Lorem laboris sunt exercitation commodo
+	non ullamco. Aute eiusmod ullamco qui commodo aliqua cillum non voluptate sunt culpa. Cillum
+	pariatur mollit elit et aute quis dolor ipsum cillum ea nisi ea elit. Qui esse incididunt duis
+	ipsum nulla commodo et ea enim non anim nisi eiusmod cillum et. Mollit ipsum excepteur sint
+	consectetur duis ipsum aute.`),w=o(),r=a("img"),H=o(),b=a("h2"),S=s("Lorem, ipsum dolor sit amet consectetur adipisicing elit"),M=o(),h=a("p"),V=s(`Dolor eu occaecat voluptate nisi et eiusmod elit. Ut duis anim veniam labore sint est nisi labore
+	tempor elit in Lorem. Veniam consectetur dolore irure amet nulla veniam quis. Ex duis nulla anim
+	labore qui pariatur pariatur ipsum minim consectetur nisi in. Quis anim aliqua cillum deserunt eu
+	nisi elit eiusmod magna qui anim minim laboris est ad. Veniam in occaecat enim eiusmod eiusmod
+	cupidatat commodo eiusmod mollit consequat reprehenderit exercitation dolore ex veniam. Dolor
+	officia nisi dolore dolor velit excepteur tempor labore aute ad enim. Quis eiusmod sit nulla do
+	tempor eu irure irure consequat voluptate laborum irure reprehenderit.`),P=o(),_=a("p"),A=s(`Laborum culpa culpa reprehenderit Lorem consequat Lorem culpa laborum occaecat non Lorem commodo
+	ullamco ex labore. In eu voluptate aliquip nisi eu. Magna eu exercitation laborum eu veniam id
+	ullamco. Dolor incididunt aliqua exercitation aliquip officia sit. Exercitation et mollit
+	exercitation in irure culpa. Fugiat veniam cupidatat qui nostrud non sit adipisicing est
+	consectetur exercitation Lorem est aliqua mollit commodo. Culpa sunt ad cupidatat adipisicing qui
+	aliquip dolor esse pariatur qui id cillum laborum ullamco.`),y=o(),L=a("h3"),g=s("Lorem ipsum dolor sit amet consectetur adipisicing elit"),I=o(),m=a("img"),G=o(),E=a("p"),F=s(`Qui enim labore duis nostrud ea labore eiusmod esse. Mollit fugiat aliquip velit culpa elit velit
+	et reprehenderit irure sit quis anim ex Lorem. Culpa do magna officia tempor proident labore
+	occaecat tempor eu deserunt cillum enim pariatur. Do occaecat voluptate minim in quis voluptate
+	aute magna. Dolore occaecat labore dolore magna aliqua. Cillum dolore quis nulla exercitation
+	mollit ut. Lorem sunt aliquip ipsum quis sint.`),Q=o(),C=a("p"),T=s(`Magna reprehenderit aliquip consectetur quis incididunt duis occaecat sint do aliquip velit
+	proident consectetur. Exercitation quis exercitation aliqua occaecat voluptate voluptate qui et
+	consectetur sit laborum velit proident. Incididunt proident duis ex ad culpa reprehenderit
+	incididunt. Enim non et duis irure in tempor sit incididunt ad minim et anim excepteur dolor elit.
+	Proident consectetur velit pariatur deserunt amet ullamco ipsum eiusmod dolore qui. Amet
+	reprehenderit voluptate est. In aliquip dolore irure cupidatat occaecat.`),this.h()},l(i){ui("svelte-132nxez",document.head).forEach(e),f=l(i),v=n(i,"H1",{});var k=c(v);j=p(k,"How to deploy a SvelteKit website to GitHub pages"),k.forEach(e),D=l(i),x=n(i,"P",{});var z=c(x);K=p(z,`Dolore ipsum esse voluptate. Culpa officia minim duis enim Lorem laboris sunt exercitation commodo
+	non ullamco. Aute eiusmod ullamco qui commodo aliqua cillum non voluptate sunt culpa. Cillum
+	pariatur mollit elit et aute quis dolor ipsum cillum ea nisi ea elit. Qui esse incididunt duis
+	ipsum nulla commodo et ea enim non anim nisi eiusmod cillum et. Mollit ipsum excepteur sint
+	consectetur duis ipsum aute.`),z.forEach(e),w=l(i),r=n(i,"IMG",{src:!0,alt:!0,height:!0,width:!0}),H=l(i),b=n(i,"H2",{});var B=c(b);S=p(B,"Lorem, ipsum dolor sit amet consectetur adipisicing elit"),B.forEach(e),M=l(i),h=n(i,"P",{});var J=c(h);V=p(J,`Dolor eu occaecat voluptate nisi et eiusmod elit. Ut duis anim veniam labore sint est nisi labore
+	tempor elit in Lorem. Veniam consectetur dolore irure amet nulla veniam quis. Ex duis nulla anim
+	labore qui pariatur pariatur ipsum minim consectetur nisi in. Quis anim aliqua cillum deserunt eu
+	nisi elit eiusmod magna qui anim minim laboris est ad. Veniam in occaecat enim eiusmod eiusmod
+	cupidatat commodo eiusmod mollit consequat reprehenderit exercitation dolore ex veniam. Dolor
+	officia nisi dolore dolor velit excepteur tempor labore aute ad enim. Quis eiusmod sit nulla do
+	tempor eu irure irure consequat voluptate laborum irure reprehenderit.`),J.forEach(e),P=l(i),_=n(i,"P",{});var O=c(_);A=p(O,`Laborum culpa culpa reprehenderit Lorem consequat Lorem culpa laborum occaecat non Lorem commodo
+	ullamco ex labore. In eu voluptate aliquip nisi eu. Magna eu exercitation laborum eu veniam id
+	ullamco. Dolor incididunt aliqua exercitation aliquip officia sit. Exercitation et mollit
+	exercitation in irure culpa. Fugiat veniam cupidatat qui nostrud non sit adipisicing est
+	consectetur exercitation Lorem est aliqua mollit commodo. Culpa sunt ad cupidatat adipisicing qui
+	aliquip dolor esse pariatur qui id cillum laborum ullamco.`),O.forEach(e),y=l(i),L=n(i,"H3",{});var X=c(L);g=p(X,"Lorem ipsum dolor sit amet consectetur adipisicing elit"),X.forEach(e),I=l(i),m=n(i,"IMG",{src:!0,alt:!0,height:!0,width:!0}),G=l(i),E=n(i,"P",{});var N=c(E);F=p(N,`Qui enim labore duis nostrud ea labore eiusmod esse. Mollit fugiat aliquip velit culpa elit velit
+	et reprehenderit irure sit quis anim ex Lorem. Culpa do magna officia tempor proident labore
+	occaecat tempor eu deserunt cillum enim pariatur. Do occaecat voluptate minim in quis voluptate
+	aute magna. Dolore occaecat labore dolore magna aliqua. Cillum dolore quis nulla exercitation
+	mollit ut. Lorem sunt aliquip ipsum quis sint.`),N.forEach(e),Q=l(i),C=n(i,"P",{});var W=c(C);T=p(W,`Magna reprehenderit aliquip consectetur quis incididunt duis occaecat sint do aliquip velit
+	proident consectetur. Exercitation quis exercitation aliqua occaecat voluptate voluptate qui et
+	consectetur sit laborum velit proident. Incididunt proident duis ex ad culpa reprehenderit
+	incididunt. Enim non et duis irure in tempor sit incididunt ad minim et anim excepteur dolor elit.
+	Proident consectetur velit pariatur deserunt amet ullamco ipsum eiusmod dolore qui. Amet
+	reprehenderit voluptate est. In aliquip dolore irure cupidatat occaecat.`),W.forEach(e),this.h()},h(){document.title="How to deploy a SvelteKit website to GitHub pages",Y(r.src,Z="https://fastly.picsum.photos/id/251/800/400.jpg?hmac=y13CO5nUuKwgLH412DjCCVGCu0MoeZSX5F3wcwKnvHM")||d(r,"src",Z),d(r,"alt","Random mountains from picsum.photos"),d(r,"height","400"),d(r,"width","800"),Y(m.src,R="https://fastly.picsum.photos/id/943/800/400.jpg?hmac=foGlp-ycjpBjdGRTPlPTZjT8e70-50L_Zeu0AiHqVnI")||d(m,"src",R),d(m,"alt","Dear in the forest from picsum.photos"),d(m,"height","400"),d(m,"width","800")},m(i,t){u(i,f,t),u(i,v,t),q(v,j),u(i,D,t),u(i,x,t),q(x,K),u(i,w,t),u(i,r,t),u(i,H,t),u(i,b,t),q(b,S),u(i,M,t),u(i,h,t),q(h,V),u(i,P,t),u(i,_,t),q(_,A),u(i,y,t),u(i,L,t),q(L,g),u(i,I,t),u(i,m,t),u(i,G,t),u(i,E,t),q(E,F),u(i,Q,t),u(i,C,t),q(C,T)},p:U,i:U,o:U,d(i){i&&e(f),i&&e(v),i&&e(D),i&&e(x),i&&e(w),i&&e(r),i&&e(H),i&&e(b),i&&e(M),i&&e(h),i&&e(P),i&&e(_),i&&e(y),i&&e(L),i&&e(I),i&&e(m),i&&e(G),i&&e(E),i&&e(Q),i&&e(C)}}}class li extends ii{constructor(f){super(),ei(this,f,null,oi,ti,{})}}export{li as component};
---
+++ _app/immutable/nodes/3.7516c79c.js
@@ -0,0 +1,49 @@
+import{S as J,i as O,s as X,a as l,k as n,q as b,J as z,h as e,c as r,l as m,m as h,r as _,K as k,n as s,b as u,D as L,H as G}from"../chunks/index.fbfca894.js";function N(B){let c,p,Q,C,d,g,D,a,H,E,q,w,M,f,j,P,o,F,I,v,y,A,x,V;return{c(){c=l(),p=n("h1"),Q=b("About page"),C=l(),d=n("p"),g=b(`Dolore ipsum esse voluptate. Culpa officia minim duis enim Lorem laboris sunt exercitation commodo
+	non ullamco. Aute eiusmod ullamco qui commodo aliqua cillum non voluptate sunt culpa. Cillum
+	pariatur mollit elit et aute quis dolor ipsum cillum ea nisi ea elit. Qui esse incididunt duis
+	ipsum nulla commodo et ea enim non anim nisi eiusmod cillum et. Mollit ipsum excepteur sint
+	consectetur duis ipsum aute.`),D=l(),a=n("img"),E=l(),q=n("p"),w=b(`Dolor eu occaecat voluptate nisi et eiusmod elit. Ut duis anim veniam labore sint est nisi labore
+	tempor elit in Lorem. Veniam consectetur dolore irure amet nulla veniam quis. Ex duis nulla anim
+	labore qui pariatur pariatur ipsum minim consectetur nisi in. Quis anim aliqua cillum deserunt eu
+	nisi elit eiusmod magna qui anim minim laboris est ad. Veniam in occaecat enim eiusmod eiusmod
+	cupidatat commodo eiusmod mollit consequat reprehenderit exercitation dolore ex veniam. Dolor
+	officia nisi dolore dolor velit excepteur tempor labore aute ad enim. Quis eiusmod sit nulla do
+	tempor eu irure irure consequat voluptate laborum irure reprehenderit.`),M=l(),f=n("p"),j=b(`Laborum culpa culpa reprehenderit Lorem consequat Lorem culpa laborum occaecat non Lorem commodo
+	ullamco ex labore. In eu voluptate aliquip nisi eu. Magna eu exercitation laborum eu veniam id
+	ullamco. Dolor incididunt aliqua exercitation aliquip officia sit. Exercitation et mollit
+	exercitation in irure culpa. Fugiat veniam cupidatat qui nostrud non sit adipisicing est
+	consectetur exercitation Lorem est aliqua mollit commodo. Culpa sunt ad cupidatat adipisicing qui
+	aliquip dolor esse pariatur qui id cillum laborum ullamco.`),P=l(),o=n("img"),I=l(),v=n("p"),y=b(`Qui enim labore duis nostrud ea labore eiusmod esse. Mollit fugiat aliquip velit culpa elit velit
+	et reprehenderit irure sit quis anim ex Lorem. Culpa do magna officia tempor proident labore
+	occaecat tempor eu deserunt cillum enim pariatur. Do occaecat voluptate minim in quis voluptate
+	aute magna. Dolore occaecat labore dolore magna aliqua. Cillum dolore quis nulla exercitation
+	mollit ut. Lorem sunt aliquip ipsum quis sint.`),A=l(),x=n("p"),V=b(`Magna reprehenderit aliquip consectetur quis incididunt duis occaecat sint do aliquip velit
+	proident consectetur. Exercitation quis exercitation aliqua occaecat voluptate voluptate qui et
+	consectetur sit laborum velit proident. Incididunt proident duis ex ad culpa reprehenderit
+	incididunt. Enim non et duis irure in tempor sit incididunt ad minim et anim excepteur dolor elit.
+	Proident consectetur velit pariatur deserunt amet ullamco ipsum eiusmod dolore qui. Amet
+	reprehenderit voluptate est. In aliquip dolore irure cupidatat occaecat.`),this.h()},l(i){z("svelte-1ine71f",document.head).forEach(e),c=r(i),p=m(i,"H1",{});var K=h(p);Q=_(K,"About page"),K.forEach(e),C=r(i),d=m(i,"P",{});var S=h(d);g=_(S,`Dolore ipsum esse voluptate. Culpa officia minim duis enim Lorem laboris sunt exercitation commodo
+	non ullamco. Aute eiusmod ullamco qui commodo aliqua cillum non voluptate sunt culpa. Cillum
+	pariatur mollit elit et aute quis dolor ipsum cillum ea nisi ea elit. Qui esse incididunt duis
+	ipsum nulla commodo et ea enim non anim nisi eiusmod cillum et. Mollit ipsum excepteur sint
+	consectetur duis ipsum aute.`),S.forEach(e),D=r(i),a=m(i,"IMG",{src:!0,alt:!0,height:!0,width:!0}),E=r(i),q=m(i,"P",{});var T=h(q);w=_(T,`Dolor eu occaecat voluptate nisi et eiusmod elit. Ut duis anim veniam labore sint est nisi labore
+	tempor elit in Lorem. Veniam consectetur dolore irure amet nulla veniam quis. Ex duis nulla anim
+	labore qui pariatur pariatur ipsum minim consectetur nisi in. Quis anim aliqua cillum deserunt eu
+	nisi elit eiusmod magna qui anim minim laboris est ad. Veniam in occaecat enim eiusmod eiusmod
+	cupidatat commodo eiusmod mollit consequat reprehenderit exercitation dolore ex veniam. Dolor
+	officia nisi dolore dolor velit excepteur tempor labore aute ad enim. Quis eiusmod sit nulla do
+	tempor eu irure irure consequat voluptate laborum irure reprehenderit.`),T.forEach(e),M=r(i),f=m(i,"P",{});var U=h(f);j=_(U,`Laborum culpa culpa reprehenderit Lorem consequat Lorem culpa laborum occaecat non Lorem commodo
+	ullamco ex labore. In eu voluptate aliquip nisi eu. Magna eu exercitation laborum eu veniam id
+	ullamco. Dolor incididunt aliqua exercitation aliquip officia sit. Exercitation et mollit
+	exercitation in irure culpa. Fugiat veniam cupidatat qui nostrud non sit adipisicing est
+	consectetur exercitation Lorem est aliqua mollit commodo. Culpa sunt ad cupidatat adipisicing qui
+	aliquip dolor esse pariatur qui id cillum laborum ullamco.`),U.forEach(e),P=r(i),o=m(i,"IMG",{src:!0,alt:!0,height:!0,width:!0}),I=r(i),v=m(i,"P",{});var Z=h(v);y=_(Z,`Qui enim labore duis nostrud ea labore eiusmod esse. Mollit fugiat aliquip velit culpa elit velit
+	et reprehenderit irure sit quis anim ex Lorem. Culpa do magna officia tempor proident labore
+	occaecat tempor eu deserunt cillum enim pariatur. Do occaecat voluptate minim in quis voluptate
+	aute magna. Dolore occaecat labore dolore magna aliqua. Cillum dolore quis nulla exercitation
+	mollit ut. Lorem sunt aliquip ipsum quis sint.`),Z.forEach(e),A=r(i),x=m(i,"P",{});var R=h(x);V=_(R,`Magna reprehenderit aliquip consectetur quis incididunt duis occaecat sint do aliquip velit
+	proident consectetur. Exercitation quis exercitation aliqua occaecat voluptate voluptate qui et
+	consectetur sit laborum velit proident. Incididunt proident duis ex ad culpa reprehenderit
+	incididunt. Enim non et duis irure in tempor sit incididunt ad minim et anim excepteur dolor elit.
+	Proident consectetur velit pariatur deserunt amet ullamco ipsum eiusmod dolore qui. Amet
+	reprehenderit voluptate est. In aliquip dolore irure cupidatat occaecat.`),R.forEach(e),this.h()},h(){document.title="About",k(a.src,H="https://fastly.picsum.photos/id/251/800/400.jpg?hmac=y13CO5nUuKwgLH412DjCCVGCu0MoeZSX5F3wcwKnvHM")||s(a,"src",H),s(a,"alt","Random mountains from picsum.photos"),s(a,"height","400"),s(a,"width","800"),k(o.src,F="https://fastly.picsum.photos/id/943/800/400.jpg?hmac=foGlp-ycjpBjdGRTPlPTZjT8e70-50L_Zeu0AiHqVnI")||s(o,"src",F),s(o,"alt","Dear in the forest from picsum.photos"),s(o,"height","400"),s(o,"width","800")},m(i,t){u(i,c,t),u(i,p,t),L(p,Q),u(i,C,t),u(i,d,t),L(d,g),u(i,D,t),u(i,a,t),u(i,E,t),u(i,q,t),L(q,w),u(i,M,t),u(i,f,t),L(f,j),u(i,P,t),u(i,o,t),u(i,I,t),u(i,v,t),L(v,y),u(i,A,t),u(i,x,t),L(x,V)},p:G,i:G,o:G,d(i){i&&e(c),i&&e(p),i&&e(C),i&&e(d),i&&e(D),i&&e(a),i&&e(E),i&&e(q),i&&e(M),i&&e(f),i&&e(P),i&&e(o),i&&e(I),i&&e(v),i&&e(A),i&&e(x)}}}class Y extends J{constructor(c){super(),O(this,c,null,N,X,{})}}export{Y as component};
