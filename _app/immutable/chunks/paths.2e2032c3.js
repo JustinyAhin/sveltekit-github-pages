cd358b9d8bcca665e9856d1e9751dc9ff0e4c47f
Justin Ahinon <justiny.ahinon@gmail.com>
2023-05-08 12:53:00 +0200

    Updates

---
+++ _app/immutable/chunks/paths.2e2032c3.js
@@ -0,0 +1,1 @@
+var s;const t=((s=globalThis.__sveltekit_qw5d4p)==null?void 0:s.base)??"/sveltekit-github-pages";var e;const a=((e=globalThis.__sveltekit_qw5d4p)==null?void 0:e.assets)??t;export{a,t as b};
